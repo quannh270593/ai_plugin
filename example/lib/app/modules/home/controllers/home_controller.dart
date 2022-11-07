0a5c0c111fa7c850ae60d84b803f63d817c8bd55
canhdt1 <canhdt1@kaopiz.com>
2022-11-07 17:07:25 +0700

    create AI plugin

---
+++ example/lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,63 @@
+import 'package:ai_plugin/ai_plugin.dart';
+import 'package:ai_plugin/ai_plugin_platform_interface.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+import '../views/components/pose_painter.dart';
+
+class HomeController extends GetxController {
+  final PoseDetector _poseDetector =
+      PoseDetector(options: PoseDetectorOptions());
+  bool _canProcess = true;
+  bool _isBusy = false;
+  CustomPaint? customPaint;
+  String? text;
+  int count = 0;
+
+  //TODO: Implement HomeController
+
+  final AiPlugin aiPlugin = AiPlugin();
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    aiPlugin.countCallback = (count) {
+      this.count = count;
+    };
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    _canProcess = false;
+    _poseDetector.close();
+    super.onClose();
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+
+    final poses = await _poseDetector.processImage(inputImage);
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = PosePainter(poses, inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      aiPlugin.pushPoseData(poses);
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      text = 'Poses found: ${poses.length}\n\n';
+      // TODO: set _customPaint to draw landmarks on top of image
+      customPaint = null;
+    }
+    _isBusy = false;
+    update();
+  }
+}
